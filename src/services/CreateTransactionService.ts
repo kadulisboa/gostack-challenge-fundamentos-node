ac0edb89243cd4aa59302c665139127170dfcf46
Kadu Lisboa <kadulisboa301@icloud.com>
2020-05-05 22:46:10 -0300

    Finished Challenge

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,37 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: TransactionDTO): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (['income', 'outcome'].includes(type) === false) {
+      throw new Error('Transaction type invalid');
+    }
+
+    if (type === 'outcome' && total < value) {
+      throw new Error("You don't enough balance");
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
